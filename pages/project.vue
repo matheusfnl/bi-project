bd316211b8940da1b1937ec324c126376ecebda4
Matheus Gabriel Goncalves Cunha De Oliveira <1362286@sga.pucminas.br>
2023-11-23 18:39:18 -0300

    add power bi

--- pages/project.vue
+++ pages/project.vue
@@ -1,3 +1,14 @@
 <template>
-  dasikdjhas
+  <div class="iframe-area">
+    <iframe title="Report Section" width="1200" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiZDM1ZTQzZDgtZmFlYi00ZjJkLWFlZTAtZWMzOTkxZDg3Nzk2IiwidCI6IjE0Y2JkNWE3LWVjOTQtNDZiYS1iMzE0LWNjMGZjOTcyYTE2MSIsImMiOjh9" frameborder="0" allowFullScreen="true"></iframe>
+  </div>
 </template>
+
+<style>
+  .iframe-area {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 98vh;
+  }
+</style>
